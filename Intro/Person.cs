08b6d591127ff330d4195b234a3a812fc394f434
Shkelzen a. Saraqini <shkelzen.saraqini2@canada.ca>
2021-04-09 14:44:16 -0400

    Added Intro project

---
+++ Intro/Person.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Intro
+{
+	public class Person
+	{
+		public Person()
+		{
+		}
+
+		public string Name { get; set; }
+
+		public DateTime DateOfBirth { get; set; }
+
+		public string CountryOfOrigin { get; set; }
+
+		public List<string> SetOfSkills { get; set; }
+
+		public int Age()
+		{
+			var now = DateTime.Now;
+			var lifeSpan = now - DateOfBirth;
+			var age = (int)(lifeSpan.TotalDays / 365);
+
+			return age;
+		}
+
+	}
+}
---
+++ Intro/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Intro
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+			var endrit = new Person();
+			endrit.Name = "Endro";
+			endrit.DateOfBirth = new DateTime(2003, 6, 4);
+
+			
+			Console.WriteLine("Hello my name is " + endrit.Name + " and I am " + endrit.Age() + " years old. Nice meeting you!");
+
+
+			var ari = new Person();
+			ari.Name = "Ari";
+			ari.DateOfBirth = new DateTime(2012, 8, 5);
+
+		
+			Console.WriteLine("Hello my name is " + ari.Name + " and I am " + ari.Age() + " years old. Nice meeting you!");
+
+		}
+	}
+}
+

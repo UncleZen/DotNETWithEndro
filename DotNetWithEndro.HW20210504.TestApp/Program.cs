14905df45c1ec0176f09ac7b205d15b123ff9d7a
BETE95 <gamingwithendrit95@gmail.com>
2021-05-11 20:27:47 +0200

    HomeWork of 05.04.2021

---
+++ DotNetWithEndro.HW20210504.TestApp/Program.cs
@@ -0,0 +1,34 @@
+﻿
+using System;
+using DotNetWithEndro.HW20210504;
+
+namespace DotNetWithEndro.HW20210504.TestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var mathhelper = new MathHelper();
+            var num1 = 50;
+            var num2 = 20;
+            var operation = 'A';
+            var result = mathhelper.Calculate(num1, num2, operation);
+            
+            Console.WriteLine($"The result of {operation}: {result}");
+
+            operation = 'D';
+            result = mathhelper.Calculate(num1, num2, operation);
+            Console.WriteLine($"The result of {operation}: {result}");
+
+            operation = 'S';
+            result = mathhelper.Calculate(num1, num2, operation);
+            Console.WriteLine($"The result of {operation}: {result}");
+
+            operation = 'M';
+            result = mathhelper.Calculate(num1, num2, operation);
+            Console.WriteLine($"The result of {operation}: {result}");
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ DotNetWithEndro.HW20210504/MathHelper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DotNetWithEndro.HW20210504
+{
+    public class MathHelper
+    {
+        public int Calculate(int number1, int number2, char operation)
+        {
+            var result = 0;
+            if(operation == 'A' || operation == 'a')
+            {
+                result = number1 + number2;
+            }
+            else if(operation == 'D' || operation == 'd')
+            {
+                result = number1 / number2; 
+            }
+            else if (operation == 'S' || operation == 's')
+            {
+                result = number1 - number2;
+            }
+            else if (operation == 'M' || operation == 'm')
+            {
+                result = number1 * number2;
+            }
+
+
+            return result;
+
+        }
+
+
+    }
+}

bf4f5168c2df705fc5dc40219ed3ea48f20ce975
BETE95 <gamingwithendrit95@gmail.com>
2021-05-17 20:36:36 +0200

    finished homework

---
+++ DotNetWithEndro.HW20210517.TestApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using DotNetWithEndro.HW20210517;
+
+
+namespace DotNetWithEndro.HW20210517.TestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var listhelper = new ListHelper();
+
+            var myarray = new int[5]
+            {
+                3,6,9,12,22
+            };
+
+            var sum = listhelper.SumOfAllItems(myarray);
+            var max = listhelper.LargestNumber(myarray);
+            var min = listhelper.SmallestNumber(myarray);
+
+            Console.WriteLine($"Sum:{sum}, Max:{max}, Min:{min}");
+
+
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ DotNetWithEndro.HW20210517/ListHelper.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DotNetWithEndro.HW20210517
+{
+    public class ListHelper
+    {
+        public int SumOfAllItems(int[] numbers)
+        {
+            var sum = 0;
+            foreach (var item in numbers)
+            {
+                sum = sum + item;
+
+            }
+
+            return sum;
+
+        }
+
+        public int LargestNumber(int[] numbers)
+        {
+
+            var max = numbers[0];
+            foreach (var item in numbers)
+            {
+                if (max < item)
+                {
+                    max = item;
+                }
+            } 
+            return max;
+
+
+
+
+        }
+        public int SmallestNumber(int[] numbers)
+        {
+
+            var min = numbers[0];
+            foreach (var item in numbers)
+            {
+                if (min > item)
+                {
+                    min = item;
+                }           
+            }
+            return min;
+
+        }
+
+    }
+}
